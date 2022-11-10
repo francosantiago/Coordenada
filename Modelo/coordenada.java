7ba749a57dd74a009c1e4058177b4693e3001d7a
Santiago Darío Franco Ruiz <102566762+francosantiago@users.noreply.github.com>
2022-11-10 08:47:38 -0500

    francosantiago

---
+++ Modelo/coordenada.java
@@ -0,0 +1,77 @@
+package Modelo;
+
+public class coordenada
+{
+    
+    private double x;
+    private double y;
+
+    public coordenada()
+    {
+        this.x = 0;
+        this.y = 0;
+    }
+
+    public coordenada( double x, double y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX()
+    {
+        return x;
+    }
+    public void setX(double x)
+    {
+        this.x = x;
+    }
+    public double getY()
+    {
+        return y;
+    
+    }
+    public void setY(double y)
+    {
+        this.y = y;
+    }
+
+    public String toString()
+    {
+        return "(" + x + "," + y + ")";
+    }
+
+    public boolean equals(Object obj)
+    {
+        coordenada otra = (coordenada)obj;
+        return (x==otra.x) && (y==otra.y);
+    }
+    
+    
+    public double Distancia(double x, double y, double x2, double y2)
+    {
+        this.x = x;
+        this.y = y;
+        this.x2 = x2;
+        this.y2 = y2;
+         if(x<x2 & y<y2)
+        {
+            d = Math.sqrt(((x2-x)(x2-x))+((y2-y)(y2-y)));
+        }
+        else if(x>x2 & y<y2)
+        {
+            d = Math.sqrt(((x-x2)(x-x2))+((y2-y)(y2-y)));
+        }
+        else if(x>x2 & y>y2)
+        {
+            d = Math.sqrt(((x-x2)(x-x2))+((y-y2)(y-y2)));
+        }
+        else
+        {
+            d = Math.sqrt(((x2-x)(x2-x))+((y-y2)(y-y2)));
+        }
+        return d;
+    }
+
+
+}
---
+++ ejecutable/Test.java
@@ -0,0 +1,41 @@
+package ejecutable;
+
+import Modelo.coordenada;
+
+public class Test 
+{
+        public static void main(String[] args) 
+        {
+            
+            coordenada c1 = new coordenada();
+            c1.setX(0);
+            c1.setY(0);
+
+            System.out.println("C1: " + c1);
+
+            coordenada c2 = new coordenada(1.3,1.5);
+
+            System.out.println("C2: "+ c2);
+            
+            coordenada c3 = c1;
+
+            System.out.println("C3: "+ c3);
+            
+            coordenada c4 = c2;
+
+            System.out.println("C4: "+ c4);
+
+        
+        if (c1.equals(c3))
+        {
+             System.out.println("las coordenadas son iguales. ");
+        }
+        else
+        {
+            System.out.println("Las coordenadas son diferentes.");
+        }    
+            
+        }
+
+
+}
